072f0aa04f49594d8032ed0eaf290eb08f3e54d4
João Marcelo Dantas <jmgrd98@gmail.com>
2022-12-24 09:32:30 -0300

    not found commit

--- js/script.js
+++ js/script.js
@@ -9,18 +9,28 @@ const baseUrl = 'https://pokeapi.co/api/v2/'
 
 async function fetchPokemon(pokemon){
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
-    return data;
+    
+    if(APIResponse.status == 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
 }
 
 async function renderPokemon(pokemon){
     const data = await fetchPokemon(pokemon);
+    
+    
+    if(data){
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = `${data.id}-`;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
 
     input.value = '';
 }
+    else{
+        pokemonName.innerHTML = 'Not found :('
+        pokemonNumber = 'Not found :('
+    }}
 
 form.addEventListener('submit', () => {
     event.preventDefault();
