af0e47f80e15f94bf7b52819f95896c07dd5c004
João Marcelo Dantas <jmgrd98@gmail.com>
2022-12-23 21:50:28 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-const pokemon = document.querySelector('.pokemon_image')
+const pokemonImage = document.querySelector('.pokemon_image')
+const pokemonName = document.querySelector('.pokemon_name')
+const pokemonNumber = document.querySelector('.pokemon_number')
 const input = document.querySelector('.input_search')
-const baseUrl = 'ttps://pokeapi.co/api/v2/'
+const baseUrl = 'https://pokeapi.co/api/v2/'
+
+async function fetchPokemon(pokemon){
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    return data;
+}
+
+async function renderPokemon(pokemon){
+    const data = await fetchPokemon(pokemon);
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = `${data.id}-`;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+}
+
+renderPokemon(3)
